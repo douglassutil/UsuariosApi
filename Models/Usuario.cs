b02d08512249307fdd8527e4bb3f8f03acf2c740
Douglas Campos Sutil <douglassutil@gmail.com>
2024-02-09 17:48:19 -0300

    Initial commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Models
+{
+    public class Usuario : IdentityUser
+    {
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        public Usuario() : base() { }
+    }
+}
